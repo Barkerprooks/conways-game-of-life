bd706bde160735ce3f9ae4bbb5ca29496266f848
parker <jonparkerbrooks@gmail.com>
2020-04-14 14:49:00 -0500

    added "clean" to build options

--- build.sh
+++ build.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 
-if [ -f "/usr/include/curses.h" ] || ([ $# -gt 1 ] && [ $1 == "-f" ]); 
-then
+function build() {
 	mkdir -p bin
-	gcc -o bin/game-of-life src/main.c -l curses
-	ln -sf $PWD/bin/game-of-life $PWD/life
+	gcc -o bin/game-of-life_$(uname -m).bin src/main.c -l curses
+	ln -sf $PWD/bin/game-of-life $PWD/run
+}
+
+if [[ $1 == '-c' || $1 == '--clean' ]];
+then
+	rm -rf bin
+	rm -rf run
+	exit	
+fi
+
+if [ -f '/usr/include/curses.h' ]; 
+then
+	build
+
+elif [[ $1 == '-f' || $1 == '--force' ]];
+then
+	echo "curses not detected on this system, trying to build anyway"
+	build
 else
-	echo "curses not detected on this system, use -f to force build"
+	echo "curses not detected on this system, use -f to force"
 fi
